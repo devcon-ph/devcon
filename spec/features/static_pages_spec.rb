edf2133cd28c3ce0fffae14d009d116a5827987f
Bryan Bibat <bry@bryanbibat.net>
2015-04-08 17:37:28 +0800

    Fix Get Involved

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -49,6 +49,76 @@ describe 'Static pages', :type => :feature do
     expect(page).to have_page_title 'About DevCon'
   end
 
+  describe 'About page' do
+    before { visit about_path }
+    let(:heading)    { 'About DevCon' }
+    let(:page_title) { 'About DevCon' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Officers page' do
+    before { visit officers_path }
+    let(:heading)    { 'DevCon Officers' }
+    let(:page_title) { 'DevCon Officers' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Regional Chapters page' do
+    before { visit chapters_path }
+    let(:heading)    { 'Regional Chapters' }
+    let(:page_title) { 'Regional Chapters' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'About DevCon Events page' do
+    before { visit events_main_path }
+    let(:heading)    { 'DevCon Events' }
+    let(:page_title) { 'About DevCon Events' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Request Event page' do
+    before { visit events_request_path }
+    let(:heading)    { 'Request an Event' }
+    let(:page_title) { 'Request an Event' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Get Involved page' do
+    before { visit get_involved_path }
+    let(:heading)    { 'Get Involved' }
+    let(:page_title) { 'Get Involved' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Volunteer page' do
+    before { visit volunteer_path }
+    let(:heading)    { 'Become a Volunteer' }
+    let(:page_title) { 'Become a Volunteer' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  describe 'Sponsor page' do
+    before { visit sponsor_path }
+    let(:heading)    { 'Sponsor Events' }
+    let(:page_title) { 'Sponsor Events' }
+
+    it_should_behave_like 'all static pages'
+  end
+  describe 'Keep in Touch page' do
+    before { visit notifications_path }
+    let(:heading)    { 'Keep in Touch' }
+    let(:page_title) { 'Keep in Touch' }
+
+    it_should_behave_like 'all static pages'
+  end
   describe '404 routing' do
     before { visit '/test' }
     let(:heading)    { 'Error 404' }
