8f29142fd3600028b62701b7ea761c7aa159fe4c
Bryan Bibat <bry@bryanbibat.net>
2016-01-10 09:03:36 +0800

    Set Closure Compiler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :closure
   # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
